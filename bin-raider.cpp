f06c245b5ff4358e23d250c43d91b3da714bfbb0
phokur <focker_i81icu812@hotmail.com>
2013-10-10 22:56:10 -0400

    Update bin-raider.cpp
    Added some comments.

--- bin-raider.cpp
+++ bin-raider.cpp
@@ -1,10 +1,19 @@
+/*
+ * Bin-Raider v0.0
+ * Program will return a list of all executables the system
+ * has in its numerous paths.
+ * */
+
 #include <string>
 #include <iostream>
 #include <stdio.h>
 
+// Run command and return result as a string
 std::string exec(std::string command)
 {
+	// Get const char* from input string command (suppress warnings)
 	const char* cmd = command.c_str();
+	// Run input command and return result as a string
     FILE* pipe = popen(cmd, "r");
     if (!pipe) return "ERROR";
     char buffer[128];
@@ -17,34 +26,38 @@ std::string exec(std::string command)
     return result;
 }
 
+// Given a delimited string, parse the strings into a string array
 void parsePaths(std::string input, std::string * pathList)
 {
 	int index = 0;
 	std::string delimiter = ":";
 	size_t pos = 0;
-	std::string token;
+	// Loop through input and insert delimited values into pathList
 	while ((pos = input.find(delimiter)) != std::string::npos)
 	{
-		token = input.substr(0, pos);
-		//std::cout << token << std::endl;
+		pathList[index] = input.substr(0, pos);
 		input.erase(0, pos + delimiter.length());
-		pathList[index] = token;
 		index++;
 	}
-	//std::cout << input << std::endl;
+	// Get the final delimited value
+	pathList[index] = input;
 }
 
+// Main method
 int main ()
 {
+	// Array to hold system paths
 	std::string pathList [16];
-	
+	// Result of $Path (colon delimited string)
 	std::string paths = exec("echo \"$PATH\"");
-	//std::cout << paths << std::endl;
+	// Parse delimited string into pathList array
 	parsePaths(paths, pathList);
-	
-	for (unsigned int i=0; i < sizeof(pathList); i++)
+	// Loop through pathList
+	for (unsigned int i=0; i < 16; i++)
 	{
-		//std::cout << pathList[i] << std::endl;
+		// Print path
+		std::cout << pathList[i] << std::endl;
+		// Print ll for path
 		std::string command = "ls -l ";
 		command.append(pathList[i]);
 		std::cout << exec(command) << std::endl;
@@ -52,3 +65,4 @@ int main ()
 	
 	return 0;
 }
+
